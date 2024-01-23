6165ed3ca38332d5bb5f8f85f359d104cb7ac295
Alessio-S <alexsat1810@gmail.com>
2024-01-23 20:26:37 +0100

    Project Files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Babel
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
